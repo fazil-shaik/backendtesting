09579f0f14c0df4904847633d21e57d84d908e45
shaik fazil basha <ammia4770@gmail.com>
2025-10-16 11:01:23 +0530

    added post routes and understanding it using postman

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,18 @@ import express from 'express';
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
+
+app.post('/data',(req,res)=>{
+    var data = req.body;
+    console.log(data);
+    res.send('POST request to the homepage');
+})
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
