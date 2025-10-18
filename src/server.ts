98e3b5d97a8bbc51073a836780b1068207549fe8
shaik fazil basha <ammia4770@gmail.com>
2025-10-18 09:09:11 +0530

    basic web sockets implementation understand about server and client implemenetation

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,27 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import Connect from './db/dbconnect.js';
+import {WebSocketServer} from 'ws'
 
 Connect();
 import router from './routes/authroutes.js';
 const app = express();
 const PORT = 3000;
+
+import http from 'http';
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello from HTTP server');
+});
+
+const wss = new WebSocketServer({ server });
+
+wss.on('connection', (ws) => {
+  ws.send('Connected via WebSocket');
+});
+
+server.listen(8000, () => console.log('Listening on port 8000'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
