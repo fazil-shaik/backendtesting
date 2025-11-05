51692809a3531a6d59b7967fca14f93521c2a771
shaik fazil basha <ammia4770@gmail.com>
2025-11-05 10:59:41 +0530

    added the frontend part withe  mouse tracking component

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+// import MouseTracker from './Components/MouseTracker'
+function App() {
+
+  return (
+    <>
+    {/* <MouseTracker render={({x, y})=>(
+      <h1>The mouse position is ({x}, {y})</h1>
+    )}/> */}
+    </>
+  )
+}
+
+export default App
